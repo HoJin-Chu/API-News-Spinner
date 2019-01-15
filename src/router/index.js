813f93eb343e3ab61a6bbbd6c4c3bd53e840ca4d
hojin-chu <cnghwls12@naver.com>
2019-01-15 20:17:04 +0900

    router

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import News from '../views/News'
+import Ask from '../views/Ask'
+import Jobs from '../views/Jobs'
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes:[
+        {
+          path: '/',
+          redirect : '/news'
+        },
+        {
+            path: '/news',
+            component: News,
+        },
+        {
+            path: '/ask',
+            component: Ask,
+        },
+        {
+            path: '/jobs',
+            component: Jobs,
+        }
+    ]
+})
---
+++ vue.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    lintOnSave : false
+} // eslint 컴파일 오류체크를 해제한다 
